ffc615a2b54e5eaaf7d3021547e0db8ed2c0252c
rafael madrigal <rfgmadrigal@gmail.com>
2024-04-07 20:40:18 -0400

    fix: attempt to resolve streamlit error

--- chat/tts.py
+++ chat/tts.py
@@ -37,18 +37,3 @@ def text_to_speech(text_to_convert):
 
     return audio_file_path
 
-
-
-# def autoplay_audio(file_path: str):
-#     with open(file_path, "rb") as f:
-#         data = f.read()
-#         b64 = base64.b64encode(data).decode()
-#         md = f"""
-#             <audio controls autoplay="true">
-#             <source src="data:audio/mp3;base64,{b64}" type="audio/mp3">
-#             </audio>
-#             """
-#         st.markdown(
-#             md,
-#             unsafe_allow_html=True,
-#         )
